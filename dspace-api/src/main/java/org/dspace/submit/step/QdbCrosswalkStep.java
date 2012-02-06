cdd4ed0dc0ac76cad55b5dcd1d769a1efd50d1b7
Villu Ruusmann <villu.ruusmann@gmail.com>
2012-02-06 17:37:16 +0200

    Dublin Core and QsarDB metadata collection step

---
+++ dspace-api/src/main/java/org/dspace/submit/step/QdbCrosswalkStep.java
@@ -0,0 +1,52 @@
+package org.dspace.submit.step;
+
+import java.io.*;
+import java.sql.*;
+
+import javax.servlet.http.*;
+
+import org.qsardb.model.*;
+
+import org.dspace.app.util.*;
+import org.dspace.authorize.*;
+import org.dspace.content.*;
+import org.dspace.core.*;
+import org.dspace.submit.*;
+
+public class QdbCrosswalkStep extends AbstractProcessingStep {
+
+	@Override
+	public int getNumberOfPages(HttpServletRequest request, SubmissionInfo submissionInfo){
+		return 1;
+	}
+
+	@Override
+	public int doProcessing(Context context, HttpServletRequest request, HttpServletResponse response, SubmissionInfo submissionInfo) throws AuthorizeException, IOException, SQLException {
+		final
+		Item item = submissionInfo.getSubmissionItem().getItem();
+
+		QdbCallable<Object> callable = new QdbCallable<Object>(){
+
+			@Override
+			public Object call(Qdb qdb){
+				QdbUtil.collectMetadata(item, qdb);
+
+				return null;
+			}
+		};
+
+		try {
+			QdbUtil.invoke(context, item, callable);
+		} catch(IOException ioe){
+			throw ioe;
+		} catch(Exception e){
+			throw new RuntimeException(e);
+		}
+
+		item.update();
+
+		context.commit();
+
+		return STATUS_COMPLETE;
+	}
+}
