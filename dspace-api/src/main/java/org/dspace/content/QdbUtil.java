46cb81e68e6902f164173488b2ba7279ec0c3c96
Villu Ruusmann <villu.ruusmann@gmail.com>
2012-02-06 11:43:18 +0200

    Collecting Dublin Core and QsarDB metadata from QDB archives

--- dspace-api/src/main/java/org/dspace/content/QdbUtil.java
+++ dspace-api/src/main/java/org/dspace/content/QdbUtil.java
@@ -1,6 +1,9 @@
 package org.dspace.content;
 
 import java.sql.*;
+import java.util.*;
+
+import org.qsardb.model.*;
 
 import org.dspace.core.*;
 
@@ -17,5 +20,50 @@ public class QdbUtil {
 		}
 
 		return format;
+	}
+
+	static
+	public void collectMetadata(Item item, Qdb qdb){
+		collectDublinCoreMetadata(item, qdb);
+		collectQsarDBMetadata(item, qdb);
+	}
+
+	static
+	public void collectDublinCoreMetadata(Item item, Qdb qdb){
+		Archive archive = qdb.getArchive();
+
+		item.addMetadata("dc", "title", null, null, archive.getName());
+	}
+
+	static
+	public void collectQsarDBMetadata(Item item, Qdb qdb){
+		List<String> endpointValues = new ArrayList<String>();
+		List<String> speciesValues = new ArrayList<String>();
+
+		PropertyRegistry properties = qdb.getPropertyRegistry();
+		for(Property property : properties){
+			String endpoint = property.getEndpoint();
+			if(endpoint != null){
+				endpointValues.add(endpoint);
+			}
+
+			String species = property.getSpecies();
+			if(species != null){
+				speciesValues.add(species);
+			}
+		}
+
+		if(endpointValues.size() > 0){
+			item.addMetadata("qdb", "property", "endpoint", null, toArray(endpointValues));
+		} // End if
+
+		if(speciesValues.size() > 0){
+			item.addMetadata("qdb", "property", "species", null, toArray(speciesValues));
+		}
+	}
+
+	static
+	private String[] toArray(List<String> strings){
+		return strings.toArray(new String[strings.size()]);
 	}
 }
