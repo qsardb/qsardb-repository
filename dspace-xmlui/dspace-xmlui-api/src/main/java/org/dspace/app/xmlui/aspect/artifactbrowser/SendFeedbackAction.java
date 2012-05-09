1172c5fdc34f4d3c79289fa0edbecd5ddf97efcd
Villu Ruusmann <villu.ruusmann@gmail.com>
2012-05-09 12:30:01 +0300

    Fixed host name extraction

--- dspace-xmlui/dspace-xmlui-api/src/main/java/org/dspace/app/xmlui/aspect/artifactbrowser/SendFeedbackAction.java
+++ dspace-xmlui/dspace-xmlui-api/src/main/java/org/dspace/app/xmlui/aspect/artifactbrowser/SendFeedbackAction.java
@@ -81,7 +81,12 @@ public class SendFeedbackAction extends AbstractAction
             // cut off all but the hostname, to cover cases where more than one URL
             // arrives at the installation; e.g. presence or absence of "www"
             int lastDot = host.lastIndexOf('.');
-            basicHost = host.substring(host.substring(0, lastDot).lastIndexOf('.') + 1);
+            lastDot = host.substring(0, lastDot).lastIndexOf('.');
+            if (lastdot < 0){
+                basicHost = host;
+            } else{
+                basicHost = host.substring(lastDot);
+            }
         }
 
         if ((fromPage == null) || ((fromPage.indexOf(basicHost) == -1) && (!validReferral)))
