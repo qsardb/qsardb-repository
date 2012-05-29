852d8031aceae23bd18c18480e320e87b4273251
Villu Ruusmann <villu.ruusmann@gmail.com>
2012-05-29 15:07:31 +0300

    Updated URL mapping for the GWT service servlet

--- dspace-xmlui/dspace-xmlui-client-gwt/src/main/java/org/dspace/rpc/gwt/QdbServiceAsync.java
+++ dspace-xmlui/dspace-xmlui-client-gwt/src/main/java/org/dspace/rpc/gwt/QdbServiceAsync.java
@@ -15,7 +15,7 @@ public interface QdbServiceAsync {
 
 	void evaluateModel(String handle, String modelId, Map<String, String> parameters, AsyncCallback<String> callback) throws DSpaceException;
 
-	public static final ServiceManager<QdbServiceAsync> MANAGER = new ServiceManager<QdbServiceAsync>("/repository/rpc/service"){
+	public static final ServiceManager<QdbServiceAsync> MANAGER = new ServiceManager<QdbServiceAsync>("/repository/service/gwt"){
 
 		@Override
 		protected Object createObject(){
