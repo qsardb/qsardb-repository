6e09d79d3e44c4a8562eb698172eb3b31b8f16e4
Villu Ruusmann <villu.ruusmann@gmail.com>
2012-06-17 20:51:00 +0300

    Fixed Widget hierarchy problem which prevented ScatterPlot instances from responding to SeriesDisplayEvent

--- dspace-xmlui/dspace-xmlui-client-gwt/src/main/java/org/dspace/client/gwt/PlotPanel.java
+++ dspace-xmlui/dspace-xmlui-client-gwt/src/main/java/org/dspace/client/gwt/PlotPanel.java
@@ -6,8 +6,12 @@ import com.google.gwt.user.client.ui.*;
 
 public class PlotPanel extends FlowPanel implements SeriesDisplayEventHandler {
 
+	private List<QdbPlot> plots = new ArrayList<QdbPlot>();
+
+
 	@Override
 	public void add(Widget widget){
+		addPlot(widget);
 
 		if(widget instanceof ScatterPlot){
 			ScatterPlot plot = (ScatterPlot)widget;
@@ -26,16 +30,40 @@ public class PlotPanel extends FlowPanel implements SeriesDisplayEventHandler {
 	}
 
 	@Override
+	public boolean remove(Widget widget){
+
+		if(widget instanceof SimplePanel){
+			SimplePanel panel = (SimplePanel)widget;
+
+			removePlot(panel.getWidget());
+		} else
+
+		{
+			removePlot(widget);
+		}
+
+		return super.remove(widget);
+	}
+
+	private void addPlot(Widget widget){
+
+		if(widget instanceof QdbPlot){
+			this.plots.add((QdbPlot)widget);
+		}
+	}
+
+	private void removePlot(Widget widget){
+
+		if(widget instanceof QdbPlot){
+			this.plots.remove((QdbPlot)widget);
+		}
+	}
+
+	@Override
 	public void onSeriesVisibilityChanged(SeriesDisplayEvent event){
 
-		for(Iterator<Widget> it = iterator(); it.hasNext(); ){
-			Widget widget = it.next();
-
-			if(widget instanceof QdbPlot){
-				QdbPlot plot = (QdbPlot)widget;
-
-				plot.changeSeriesVisibility(event);
-			}
+		for(QdbPlot plot : this.plots){
+			plot.changeSeriesVisibility(event);
 		}
 	}
 }
