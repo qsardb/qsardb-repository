dface05f16819d78931cdd31586735056b3b717a
Sulev Sild <sulev.sild@ut.ee>
2015-01-23 13:47:16 +0200

    Improved resolver tooltips

--- dspace-qsardb/dspace-qsardb-client-gwt/src/main/java/org/dspace/qsardb/client/gwt/ResolverTooltip.java
+++ dspace-qsardb/dspace-qsardb-client-gwt/src/main/java/org/dspace/qsardb/client/gwt/ResolverTooltip.java
@@ -1,5 +1,6 @@
 package org.dspace.qsardb.client.gwt;
 
+import com.google.gwt.dom.client.Style;
 import java.util.*;
 
 import com.google.gwt.user.client.*;
@@ -32,6 +33,9 @@ public class ResolverTooltip extends Tooltip {
 		final Map<String, String> values = resolver.resolve(resolveKey);
 		final String resolveMethod = resolver.resolveMethod(values);
 
+		resetTable(null, values, resolveMethod);
+		setPopupPositionAndShow(getPositionCallback());
+
 		ImageLoadHandler loadHandler = new ImageLoadHandler(){
 
 			@Override
@@ -45,15 +49,14 @@ public class ResolverTooltip extends Tooltip {
 						image.setPixelSize(size.getWidth(), size.getHeight());
 					}
 
-					resetTable(image, values, image != null ? resolveMethod : null);
-
-					setPopupPositionAndShow(getPositionCallback());
+					resetImage(image, resolveMethod);
 				}
 			}
 		};
 
 		String url = resolver.resolveURL(values);
 		ImagePreloader.load(url, loadHandler);
+
 	}
 
 	private void resetTable(Image image, Map<String, String> values, String method){
@@ -66,14 +69,12 @@ public class ResolverTooltip extends Tooltip {
 
 		int row = 0;
 
-		if(image != null){
-			formatter.setColSpan(row, 0, 2);
+		formatter.setColSpan(row, 0, 2);
 
-			this.table.setWidget(row, 0, image);
-			formatter.setHorizontalAlignment(row, 0, HasHorizontalAlignment.ALIGN_LEFT);
+		resetImage(image, "Resolving "+method+"...");
+		formatter.setHorizontalAlignment(row, 0, HasHorizontalAlignment.ALIGN_LEFT);
 
-			row++;
-		}
+		row++;
 
 		Set<Map.Entry<String, String>> entries = values.entrySet();
 		for(Map.Entry<String, String> entry : entries){
@@ -84,14 +85,25 @@ public class ResolverTooltip extends Tooltip {
 				continue;
 			}
 
-			this.table.setWidget(row, 0, new HTML("<b>" + key + "</b>")); // XXX
+			this.table.setWidget(row, 0, new HTML("<b>" + key + "</b>"));
 			formatter.setHorizontalAlignment(row, 0, HasHorizontalAlignment.ALIGN_LEFT);
 
-			this.table.setWidget(row, 1, new HTML((key).equals(method) ? "<u>" + value + "</u>" : value)); // XXX
+			this.table.setWidget(row, 1, new HTML(value));
 			formatter.setHorizontalAlignment(row, 1, HasHorizontalAlignment.ALIGN_LEFT);
 
 			row++;
 		}
+	}
+
+	private void resetImage(Image image, String method){
+		FlowPanel w = new FlowPanel();
+		w.setStylePrimaryName("depict");
+		if (image != null) {
+			w.add(image);
+			w.add(new InlineLabel("[ "+method+" ]"));
+		}
+
+		this.table.setWidget(0, 0, w);
 	}
 
 	public void schedule(String id, final int x, final int y){
@@ -101,7 +113,6 @@ public class ResolverTooltip extends Tooltip {
 
 	@Override
 	public void cancel(){
-		scheduledKey = null;
 		super.cancel();
 	}
 }
