f3c295140808c9465a4d211a8ec8822ef8aca5a8
Sulev Sild <sulev.sild@ut.ee>
2016-05-11 14:33:10 +0300

    Use UIBinder in CompoundInputPanel

--- dspace-qsardb/dspace-qsardb-client-gwt/src/main/java/org/dspace/qsardb/client/gwt/CompoundInputPanel.java
+++ dspace-qsardb/dspace-qsardb-client-gwt/src/main/java/org/dspace/qsardb/client/gwt/CompoundInputPanel.java
@@ -2,8 +2,12 @@ package org.dspace.qsardb.client.gwt;
 
 import java.util.*;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.*;
 import com.google.gwt.event.shared.*;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.*;
 import com.google.gwt.user.client.ui.*;
 
@@ -12,72 +16,28 @@ import org.fusesource.restygwt.client.Method;
 import org.fusesource.restygwt.client.MethodCallback;
 
 public class CompoundInputPanel extends Composite {
+	interface Binder extends UiBinder<Widget, CompoundInputPanel> {}
+	private final static Binder binder = GWT.create(Binder.class);
     
-        final TextBox textBox;
+	@UiField TextBox textBox;
+	@UiField Button calculateButton;
 
 	public CompoundInputPanel(QdbTable table){
-		Panel panel = new VerticalPanel();
+		Widget panel = binder.createAndBindUi(this);
 
 		boolean calculable = true;
-
 		List<DescriptorColumn> descriptors = table.getAllColumns(DescriptorColumn.class);
 		for(DescriptorColumn descriptor : descriptors){
 			calculable &= descriptor.isCalculable();
 		}
 
-		panel.add(new HTML("<u>Chemical structure input</u> (InChI or SMILES format):"));
-
-		textBox = new TextBox();
-		textBox.setVisibleLength(60);
 		if (!calculable) {
 			textBox.setText("Descriptors in this model require proprietary software and can not be calculated");
 			textBox.getElement().setAttribute("style", "color:gray");
 		}
 		textBox.setEnabled(calculable);
 
-		panel.add(textBox);
-
-		final
-		KeyPressHandler keyHandler = new KeyPressHandler(){
-
-			@Override
-			public void onKeyPress(KeyPressEvent event){
-				int charCode = event.getUnicodeCharCode();
-
-				// Not an alphanumeric key
-				if(charCode == 0){
-					int keyCode = (event.getNativeEvent()).getKeyCode();
-
-					if(keyCode == KeyCodes.KEY_ENTER){
-						onEnter();
-					}
-				} else
-
-				if(charCode == KeyCodes.KEY_ENTER){
-					onEnter();
-				}
-			}
-
-			private void onEnter(){
-				calculate(textBox.getText());
-			}
-		};
-		textBox.addKeyPressHandler(keyHandler);
-
-		final
-		Button button = new Button("Calculate");
-		button.setEnabled(calculable);
-
-		panel.add(button);
-
-		ClickHandler clickHandler = new ClickHandler(){
-
-			@Override
-			public void onClick(ClickEvent event){
-				calculate(textBox.getText());
-			}
-		};
-		button.addClickHandler(clickHandler);
+		calculateButton.setEnabled(false);
 
 		initWidget(panel);
 	}
@@ -86,21 +46,34 @@ public class CompoundInputPanel extends Composite {
 		return addHandler(handler, InputChangeEvent.TYPE);
 	}
 
-	private void calculate(String string){
+	@UiHandler("textBox")
+	public void handleReturnKey(KeyPressEvent event) {
+		int keyCode = event.getUnicodeCharCode();
+		if (keyCode == 0) {
+			keyCode = event.getNativeEvent().getKeyCode();
+		}
+		if (keyCode == KeyCodes.KEY_ENTER) {
+			calculate();
+		}
+	}
 
-		if(string != null){
-			string = string.trim();
+	@UiHandler("textBox")
+	public void handleTextBoxInput(KeyUpEvent event) {
+		calculateButton.setEnabled(!textBox.getValue().isEmpty());
+	}
 
-			if("".equals(string)){
-				return;
-			}
-		} else
+	@UiHandler("calculateButton")
+	public void handleClick(ClickEvent event) {
+		calculate();
+	}
 
-		{
+	public void calculate(){
+		String inputStructure = textBox.getText() != null ? textBox.getText().trim() : "";
+		if ("".equals(inputStructure)) {
 			return;
 		}
 
-		PredictorRequest request = new PredictorRequest(string);
+		PredictorRequest request = new PredictorRequest(inputStructure);
 		PredictorClient.predict(request, new MethodCallback<PredictorResponse>() {
 			@Override
 			public void onFailure(Method method, Throwable ex) {
